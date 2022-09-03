b3858e6082b17fe0106086fe78a874e0e98fe63d
David Marins <davidmarinsdiogo01@gmail.com>
2022-09-03 12:45:57 -0300

    Funções para fazer os calculos

---
+++ Calculadora.js
@@ -0,0 +1,26 @@
+var numA = 0
+var numB = 0
+numA = parseInt(prompt("Digite Número A :"))
+numB = parseInt(prompt("Digite Número B :"))
+console.log("Número A :", numA, "\nNúmero B :", numB,)
+
+function CalcularSoma(num1, num2) {
+    return num1 + num2
+}
+
+function CalcularSubtracao(num1, num2) {
+    return num1 - num2
+
+}
+
+function CalcularMultiplicacao(num1, num2) {
+    return num1 * num2
+}
+
+function CalcularDivisao(num1, num2) {
+    return num1 / num2
+
+}
+
+
+

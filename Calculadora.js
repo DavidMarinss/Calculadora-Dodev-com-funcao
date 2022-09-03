165317646b6f3e07db964f06d6d10de9d0993cf1
David Marins <davidmarinsdiogo01@gmail.com>
2022-09-03 13:40:09 -0300

    Melhorando o menu de opção do usuário para fazer os calculos.

--- Calculadora.js
+++ Calculadora.js
@@ -1,33 +1,38 @@
 var numA = 0
 var numB = 0
 var contador = true
-numA = parseInt(prompt("Digite Número A :"))
-numB = parseInt(prompt("Digite Número B :"))
-console.log("Número A :", numA, "\nNúmero B :", numB,)
+var Resultado = []
 
-while (contador){
 
-var opcao = prompt("Qual operação deseja fazer? 1 -Somar / 2-Subtrair /3-Multiplicar /4-Dividir ")
 
-if (opcao == 1) {
-    console.log("Resultado da soma é :", CalcularSoma(numA, numB))
-}
 
-if (opcao == 2) {
-    console.log("Resultado da Subtração é :", CalcularSubtracao(numA, numB))
-}
 
-if (opcao == 3) {
-    console.log("Resultado da Multiplição é :", CalcularMultiplicacao(numA, numB))
-}
+while (contador) {
+    numA = parseInt(prompt("Digite Número A :"))
+    numB = parseInt(prompt("Digite Número B :"))
+    console.log("Número A :", numA, "\nNúmero B :", numB,)
 
-if (opcao == 4) {
-    console.log("Resultado da Divisão é :", CalcularDivisao(numA, numB))
-}
-var continuar = prompt ("Deseja fazer mais Algum calculo? Aperte (ENTER) para sim e (1) para inserar.")
-if (continuar == 1){
-    contador = false
-}
+    var opcao = prompt("Qual operação deseja fazer? 1 -Somar / 2-Subtrair /3-Multiplicar /4-Dividir ")
+
+    if (opcao == 1) {
+        console.log("Resultado da soma é :" + CalcularSoma(numA, numB))
+    }
+
+    if (opcao == 2) {
+        console.log("Resultado da Subtração é :" + CalcularSubtracao(numA, numB))
+    }
+
+    if (opcao == 3) {
+        console.log("Resultado da Multiplição é :" + CalcularMultiplicacao(numA, numB))
+    }
+
+    if (opcao == 4) {
+        console.log("Resultado da Divisão é :" + CalcularDivisao(numA, numB))
+    }
+    var continuar = prompt("Deseja fazer mais Algum calculo? 1- sim / 2- não")
+    if (continuar == 2) {
+        contador = false
+    }
 
 }
 function CalcularSoma(num1, num2) {
